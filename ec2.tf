2cf6ebcfdc2959f62857f32a63f079d840cf8544
rogerslevi <levi.rogers@hackney.gov.uk>
2025-03-07 17:39:39 +0000

    re-instating resources after adding remote state

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_security_group" "WebAppSec" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["217.42.29.254/32", "72.14.201.120/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
