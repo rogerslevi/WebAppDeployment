fce951b4fe21398cd73e8b06cdb833b269d9db11
rogerslevi <levi.rogers@hackney.gov.uk>
2025-04-02 15:43:05 +0100

    adding subnets to vpc

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@
 #   instance_type          = "t2.micro"
 #   key_name               = "WebApp.pem"
 #   monitoring             = true
-#   vpc_security_group_ids = ["sg-12345678"]
+#   vpc_security_group_ids = ["sg-04644fd87e04c8aec"]
 #   subnet_id              = "subnet-eddcdzz4"
 
 #   tags = {
--- vpc.tf
+++ vpc.tf
@@ -1,23 +1,23 @@
-# resource "aws_vpc" "WebApp_VPC" {
-#   cidr_block = "10.0.0.0/24"
-#   tags = {
-#     Name = var.vpc
-#   }
-# }
+resource "aws_vpc" "WebApp_VPC" {
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = var.vpc
+  }
+}
 
-# resource "aws_subnet" "public_subnet" {
-#   vpc_id     = aws_vpc.WebApp_VPC.id
-#   cidr_block = "10.0.0.0/27"
-#   tags = {
-#     Name = "public_subnet"
-#   }
-# }
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.WebApp_VPC.id
+  cidr_block = "10.0.0.0/27"
+  tags = {
+    Name = "public_subnet"
+  }
+}
 
-# resource "aws_subnet" "private_subnet" {
-#   vpc_id     = aws_vpc.WebApp_VPC.id
-#   cidr_block = "10.0.0.32/27"
-#   tags = {
-#     Name = "private_subnet"
-#   }
-# }
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.WebApp_VPC.id
+  cidr_block = "10.0.0.32/27"
+  tags = {
+    Name = "private_subnet"
+  }
+}
 
